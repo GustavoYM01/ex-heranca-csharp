ff5acad8c93729911c8ff157c21b7fd4d635fb1a
GustavoYM01 <gumatimoto@hotmail.com>
2022-02-11 09:05:46 -0300

    First commit - added files

---
+++ heranca-1/Entities/BusinessAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace heranca_1.Entities
+{
+    class BusinessAccount : Account
+    {
+
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, 
+            double balance, double loanLimit) : base(number, 
+                holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("Loan not granted!");
+            }
+        }
+    }
+}
---
+++ heranca-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using heranca_1.Entities;
+
+namespace heranca_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010,
+                "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
